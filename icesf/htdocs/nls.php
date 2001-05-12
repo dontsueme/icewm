2422b7c0a9b81859c2e43b558a7dcd4fccc9d6ae
massel <>
2001-05-12 10:55:57 +0000

    NLS Status Report

---
+++ icesf/htdocs/nls.php
@@ -0,0 +1,8 @@
+<?php
+  define(ICEWM_TITLE,  'NLS Status Report');
+  define(ICEWM_SCRIPT, $SCRIPT_FILENAME);
+    
+  include('../libphp/head.php');
+  include('../libphp/nls.html');
+  include('../libphp/tail.php');
+?>
