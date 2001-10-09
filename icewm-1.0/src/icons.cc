c6a80658b2a2164b851a17028e2fa9b395d40bb5
massel <>
2001-10-09 13:26:09 +0000

    what a typo from me...

--- icewm-1.0/src/icons.cc
+++ icewm-1.0/src/icons.cc
@@ -259,14 +259,15 @@ YIcon::YIcon(Image * small, Image * large, Image * huge) :
 
 YIcon::~YIcon() {
     if (this != firstIcon) {
-        for (YIcon * icn(firstIcon); NULL != icn; icn->fNext) {
+        for (YIcon * icn(firstIcon); NULL != icn; icn = icn->fNext) {
             if (icn->fNext == this) {
                 icn->fNext = fNext;
                 break;
             }
         }
-    } else
+    } else {
         firstIcon = fNext;
+    }
 
     delete fHuge; fHuge = NULL;
     delete fLarge; fLarge = NULL;
