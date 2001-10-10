0b51bbeb3b6482a5a2ec6d99c911a5c6bcb36ab7
massel <>
2001-10-10 15:39:14 +0000

    updated config.sub/guess, added update-config target

--- icewm-1.0/utils/release.sh
+++ icewm-1.0/utils/release.sh
@@ -34,7 +34,7 @@ echo Running configure
 	    --enable-i18n --enable-nls
 
 echo Making distribution information
-make -s icewm.lsm icewm.spec docs
+make -s update-config icewm.lsm icewm.spec docs
 
 echo Cleaning distribution
 rm config.{cache,log,status}
